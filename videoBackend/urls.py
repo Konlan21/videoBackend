5eda96c9d69be342819e966e44b34efa81e0b87c
Konlan21 <joekonlan16@gmail.com>
2023-09-28 16:52:47 +0000

    Setup djoser library for authentication

--- videoBackend/urls.py
+++ videoBackend/urls.py
@@ -21,7 +21,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('videoupload/', include('videoupload.urls'))
+    path('videoupload/', include('videoupload.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
 
 
--- videoupload/models.py
+++ videoupload/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 
 class Video(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
+    description = models.TextField()
+    upload_date = models.DateTimeField(auto_now_add=True)
     video_file = models.FileField(upload_to='videos/')
