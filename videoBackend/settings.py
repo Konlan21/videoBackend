d9905e6bdcf85512747302d90cacb4b794640cf1
Konlan21 <joekonlan16@gmail.com>
2023-09-28 17:44:40 +0000

    Complete authentication endpoints

--- videoBackend/settings.py
+++ videoBackend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,4 +138,14 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
+}
+
+# DJOSER = {
+#     'SERIALIZERS': {
+#         'user_create': 'videoupload.serializers.UserCreateSerializer'
+#     }
+# }
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=2)
 }
--- videoupload/serializers.py
+++ videoupload/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework.serializers import ModelSerializer
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
 from .models import Video
 
+
+
+# class UserCreateSerializer(BaseUserCreateSerializer):
+#     class Meta(BaseUserCreateSerializer.Meta):
+#         fields = ['id', 'email', 'password']
 
 
 class VideoSerializer(ModelSerializer):
